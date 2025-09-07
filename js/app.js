bb4ceec6d89a1dd26656d1e18e063f6b5d437663
PierreLogs <lpierre.dev@gmail.com>
2025-09-07 06:09:29 -0500

    Add first function to project

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 let amigos = [];
+
+/*Función para agregar amigos al array amogos*/
+/*function AgregarAmigo() {
+  let nombre = prompt("Ingresa el nombre de tu amigo"); //Pide al usuario ingresar el nombre de un amigo
+  amigos.push(nombre); //Permite agregar el nombre de un amigo al array amigos
+  console.log(amigos); //muestra en la consola el nombre del amigo agregado
+}*/
+
+const AgregarAmigo = () => {
+  const nombre = prompt("Ingresa el nombre de tu amigo"); //Pide al usuario ingresar el nombre de un amigo
+
+  if (nombre) {
+    amigos.push(nombre); //Permite agregar el nombre de un amigo al array amigos
+    console.log(amigos); //muestra en la consola el nombre del amigo agregado
+    return amigos;
+  } else {
+    alert("No ingresaste ningun nombre");
+    return null;
+  }
+};
